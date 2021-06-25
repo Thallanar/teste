40e57ac93877a50609dd8e30406170d6ef27d38a
André Gomes <aluizgomes@terra.com.br>
2021-06-25 08:46:20 -0300

    just att on the .ignore

--- lib/constructor/clientes_getter.dart
+++ lib/constructor/clientes_getter.dart
@@ -5,17 +5,9 @@ import 'package:flutter/foundation.dart';
 import 'clientes.dart';
 import 'clientes_exemplos.dart';
 
-class ClientesGetter {
+class ClientesGetter with ChangeNotifier{
   var json = jsonDecode(exemplosClientes);
 
   List<Clientes> get cliente => [...json];
 
-  // List<Clientes> get procura {
-  //   return _cliente.where((client) => client.clienteProcurado).toList();
-  // }
-
-  // void addEmpresa(Clientes clients) {
-  //   _cliente.add(clients);
-  //   notifyListeners();
-  // }
 }
